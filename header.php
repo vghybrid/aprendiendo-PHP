d72a62a446a7e5b85c463f1891327e1e75e29cb6
vghybrid <anderson.sarmiento13@gmail.com>
2022-01-11 10:27:09 -0300

    cambios

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <META HTTP-EQUIV="refresh" CONTENT="15">
     <title>Prueba 2.0</title>
     <!--archivo bootstrap-->
     <link rel="stylesheet" href="./css/bootstrap/css/bootstrap.min.css">
@@ -15,10 +16,6 @@
     <link rel="stylesheet" href="./css/estilos.css">
     <!--favicon-->
     <link rel="shortcut icon" href="./images/favicon.ico">
-
-
-    <!-- Custom styles for this template -->
-    <link href="sticky-footer-navbar.css" rel="stylesheet">
 </head>
 
 <body class="d-flex flex-column h-100">
